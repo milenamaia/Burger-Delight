369274c66b896e05c1ecb71606fd24c18ddc1bfb
milenamaia <milenamaiadesouza123@gmail.com>
2023-12-05 20:54:19 -0300

    atualizando

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
+
 document.addEventListener("DOMContentLoaded", function() {
     const links = document.querySelectorAll('nav a');
 
@@ -16,4 +24,19 @@ document.addEventListener("DOMContentLoaded", function() {
             behavior: 'smooth'
         });
     }
+});
+
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.item-menu' , {duration: 2000});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerBtn = document.querySelector('.navigation');
+    const menu = document.querySelector('.nav-menu');
+
+    hamburgerBtn.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
 });
